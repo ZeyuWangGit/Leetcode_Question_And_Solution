e9e2c9136e7d3083b59a5624f7aa23eed48e1e9e
Zeyu Wang <wzy.franklin@gmail.com>
2019-08-21 22:35:59 +1000

    Solution for Backspace String Compare

---
+++ LeetCode_Solutions/QueueAndStack/BackspaceStringCompare.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCode_Solutions.QueueAndStack
+{
+    [TestClass]
+    public class BackspaceStringCompare
+    {
+        [TestMethod]
+        public void BackSpaceStringCompare_shouldPass()
+        {
+            BackspaceCompare("ab#c", "ad#c").ShouldBe(true);
+            BackspaceCompare("a##c", "#a#c").ShouldBe(true);
+            BackspaceCompare("a#c", "b").ShouldBe(false);
+        }
+        public bool BackspaceCompare(string S, string T)
+        {
+            return BackspaceStringBuilder(S).Equals(BackspaceStringBuilder(T));
+        }
+
+        public string BackspaceStringBuilder(string S)
+        {
+            Stack<char> str = new Stack<char>();
+            foreach (var c in S)
+            {
+                if (c!='#')
+                {
+                    str.Push(c);
+                }
+                else if (str.Count > 0)
+                {
+                    str.Pop();
+                }
+            }
+            return string.Join("", str.ToArray());
+        }
+    }
+}
