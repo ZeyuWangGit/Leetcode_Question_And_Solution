6d41547346d39fe0c7c6161aff37a30353265d06
Zeyu Wang <wzy.franklin@gmail.com>
2019-08-26 22:00:19 +1000

    Question - Valid Parentheses

---
+++ LeetCode_Solutions/QueueAndStack/ValidParentheses.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCode_Solutions.QueueAndStack
+{
+    [TestClass]
+    public class ValidParentheses
+    {
+        [TestMethod]
+        public void ValidParentheses_ShouldPass()
+        {
+            IsValid("()").ShouldBe(true);
+            IsValid("()[]{}").ShouldBe(true);
+            IsValid("(]").ShouldBe(false);
+            IsValid("([)]").ShouldBe(false);
+            IsValid("{[]}").ShouldBe(true);
+        }
+        public bool IsValid(string s)
+        {
+            var mapping = new Dictionary<char, char> {{')', '('}, {'}', '{'}, {']', '['}};
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (mapping.ContainsKey(c))
+                {
+                    var top = stack.Count < 1 ? '#' : stack.Pop();
+                    if (top != mapping[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            return stack.Count < 1;
+
+        }
+    }
+}
